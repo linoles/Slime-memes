5f9b26ed8671a4f9c834fd57835230c378401892
linoles <cernikovdenis081@gmail.com>
2024-12-22 22:49:13 +0300

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { PlusesCards } from "./ui/card";
 
 export default function Page() {
   return (
-    <main className="flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-b from-slate-100 to-white overflow-x-hidden">
+    <main className="flex flex-col items-center justify-center h-screen w-screen bg-gradient-to-b from-slate-50 to-white overflow-x-hidden">
       <GreetPage />
-      <div className="flex flex-col items-center justify-center relative h-fit top-full pb-20 bg-white w-full">
-        <p className="text-3xl font-medium text-slate-700">Что я могу получить?</p>
-        <div className="flex flex-col items-start justify-center w-full h-fit">
-          <PlusesCards />
-        </div>
+      <div className="flex flex-col items-center justify-center relative top-full pb-20 bg-white w-full">
+        <p className="text-3xl font-medium text-slate-700">
+          Что я могу получить?
+        </p>
+        <PlusesCards />
       </div>
     </main>
   );
--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -1,9 +1,9 @@
-'use client';
+"use client";
 
 import type { CardProps } from "@/app/lib/definitions";
 import Image from "next/image";
 import { robotoCondensed } from "@/app/ui/fonts";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export function Card({
   heading,
@@ -15,59 +15,71 @@ export function Card({
   const cardStyle = {
     backgroundColor: bg,
   };
-  
-  const funcDesc = () => {
-    const element = document.querySelector(`p.${heading.replaceAll(" ", "-")}`);
-    if (element?.classList.contains("hidden")) {
-      element.classList.remove("hidden");
-    }
-  }
-  const hideDesc = () => {
-    const element = document.querySelector(`p.${heading.replaceAll(" ", "-")}`);
-    element?.classList.add("hidden");
-  }
 
-  document.addEventListener("resize", () => {
-    setWidth(width2)
-  });
+  const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+
+  const toggleDescription1 = () => {
+    setIsDescriptionVisible(true);
+  };
+  const toggleDescription2 = () => {
+    setIsDescriptionVisible(false);
+  };
 
   const [width, setWidth] = useState(400);
   const [height, setHeight] = useState(600);
   const [width2, setWidth2] = useState(400);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(width2);
+    };
+
+    document.addEventListener("resize", handleResize);
+
+    return () => {
+      document.removeEventListener("resize", handleResize);
+    };
+  }, [width2]);
+
   return (
     <div
       style={cardStyle}
       className="flex flex-col items-center justify-start h-fit rounded-3xl ml-5 mr-5 transition ease-in-out duration-500"
-      onMouseOver={funcDesc}
-      onMouseLeave={hideDesc}
+      onMouseOver={toggleDescription1}
+      onMouseLeave={toggleDescription2}
     >
-      <Image
-        src={photoPath}
-        alt="Picture of the author"
-        width={width2}
-        height={600}
-        className={`rounded-3xl z-0 ${heading.replaceAll(" ", "-")}`}
-      />
       <div
-        style={{width: width, height: height}}
-        className={`absolute z-10 flex flex-col items-left justify-end sm:pl-6 sm:pb-6 pl-2 pb-2 transition ease-in-out duration-500 ${heading.replaceAll(" ", "-")}`}
+        style={{ width: "400px", height: "600px" }}
+        className={`absolute z-10 sm:pl-6 sm:pb-6 pl-2 pb-2 ${heading.replaceAll(
+          " ",
+          "-"
+        )}`}
       >
+        <Image
+          src={photoPath}
+          alt={heading}
+          width={width2}
+          height={height}
+          className={`absolute rounded-3xl z-0 ${heading.replaceAll(
+            " ",
+            "-"
+          )}`}
+        />
         <h4
-          className={`${robotoCondensed.className} text-2xl font-medium`}
+          className={`${robotoCondensed.className} absolute z-10 text-2xl font-medium bottom-0`}
           style={{ color: color }}
         >
           {heading.toUpperCase()}
         </h4>
-        <p className={`text-lg hidden photoDesc ${heading.replaceAll(" ", "-")}`} style={{ color: color }}>
+        <p
+          className={`absolute bottom-0 text-lg z-10 ${isDescriptionVisible ? "" : "hidden"}`}
+          style={{ color: color }}
+        >
           {description}
         </p>
       </div>
     </div>
   );
-
-
-
 }
 
 export function PlusesCards() {
